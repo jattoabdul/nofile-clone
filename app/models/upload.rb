0a84eeee3ddc749324ec3a6fa0bd93c91d04ce02
jattoabdul <jattoade@gmail.com>
2019-05-18 20:41:12 +0100

    feat: prevent upload of duplicate files and add link to all uploads

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -8,10 +8,25 @@ class Upload < ApplicationRecord
 
   validates :reference, uniqueness: { case_sensitive: true }
   before_create :set_upload_share_link
+  before_validation :sanitize_list
 
   private
 
   def set_upload_share_link
     self.link = uploads_url(self, only_path: false, host: ENV.fetch('APPLICATION_HOST'))
+  end
+
+  def sanitize_list
+    return if files.size < 2
+
+    file_set = {}
+
+    files.each do |file|
+      check = file.blob.checksum
+
+      file.purge if file_set[check].present?
+
+      file_set[check] = file.blob
+    end
   end
 end
