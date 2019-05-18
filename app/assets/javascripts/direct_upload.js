88f52ee921a4411177b684e544667303a7dbfcd5
jattoabdul <jattoade@gmail.com>
2019-05-18 21:08:16 +0100

    handle viewing file names on select of file before upload to show progress

--- app/assets/javascripts/direct_upload.js
+++ app/assets/javascripts/direct_upload.js
@@ -1,7 +1,33 @@
+document.addEventListener("turbolinks:load", event => {
+  const form = document.querySelector("#upload_files");
+  const selectedFiles = document.querySelector("#selected-files");
+  form.onchange = formEvent => {
+    Object.values(formEvent.target.files)
+      .map(file => file.name)
+      .forEach((fileName, index) => {
+        let fileEntry = document.createElement("div");
+        fileEntry.className = "direct-upload direct-upload--pending";
+        fileEntry.id = `direct-upload-${index + 1}`;
+        let progress = document.createElement("div");
+        progress.className = "direct-upload__progress";
+        progress.id = `direct-upload-progress-${index + 1}`;
+        progress.style.width = "0%";
+        let fileEntrySpan = document.createElement("span");
+        fileEntrySpan.innerHTML = fileName;
+        fileEntrySpan.className = "direct-upload__filename";
+        fileEntry.appendChild(progress);
+        fileEntry.appendChild(fileEntrySpan);
+        selectedFiles.appendChild(fileEntry);
+      });
+  };
+});
+
 addEventListener("direct-upload:initialize", event => {
+  const selectedFiles = document.querySelector("#selected-files");
   const { target, detail } = event
   const { id, file } = detail
-  target.insertAdjacentHTML("beforebegin", `
+  target.insertAdjacentHTML("beforebegin", `<span>File Uploading</span>`)
+  selectedFiles.appendChild(`
         <div id="direct-upload-${id}" class="direct-upload direct-upload--pending">
           <div id="direct-upload-progress-${id}" class="direct-upload__progress" style="width: 0%"></div>
           <span class="direct-upload__filename">${file.name}</span>
