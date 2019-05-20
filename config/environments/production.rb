83fe9b018ff8f721f687fb00270fbd1e5c2692f5
jattoabdul <jattoade@gmail.com>
2019-05-20 02:49:08 +0100

    update uglifier config and add meta tags

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,8 +23,8 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  # TODO: if it Doesn't work, comment it out or remove this line
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
